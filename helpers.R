8736619bf5d5005237513c45443369e4d748de0a
Kenneth Schackart <schackartk1@gmail.com>
2021-09-06 22:30:16 -0700

    add roxygen comments

--- helpers.R
+++ helpers.R
@@ -1,3 +1,15 @@
+#' perp_segment_coord
+#' Calculate coordinates of segments perpendicular to regression line.
+#'
+#' @param x0 List of x values
+#' @param y0 List of y values
+#' @param int Regression line intercept
+#' @param slp Regression line slope
+#'
+#' @return Coordinates
+#' @export
+#'
+#' @examples
 perp_segment_coord <- function(x0, y0, int, slp){
   x1 <- (x0 + slp*(y0 - int))/(1 + slp^2)
   y1 <- slp*x1 + int
@@ -5,22 +17,35 @@ perp_segment_coord <- function(x0, y0, int, slp){
   segments <- list(x0=x0, y0=y0, x1=x1, y1=y1)
 }
 
-plot_perp <- function(df){
+#' plot_perp
+#' Plot with ordinary least-squares regression and perpindicular segments
+#'
+#' @param df Data frame
+#' @param x X-axis variable
+#' @param y Y-axis variable
+#'
+#' @return Plot
+#' @export
+#'
+#' @examples
+plot_perp <- function(df, x, y){
   
   lm_model <- linear_reg() %>% 
     set_engine("lm") %>% 
     set_mode("regression")
   
+  form <- formula(paste(eval(y),"~", eval(x)))
+  
   lm_fit <- lm_model %>% 
-    fit(hp ~ wt, data=df)
+    fit(form, data=train)
   
   intercept <- summary(lm_fit$fit)$coefficients["(Intercept)",1]
-  slope <- summary(lm_fit$fit)$coefficients["wt",1]
+  slope <- summary(lm_fit$fit)$coefficients[x,1]
   
-  segments <- perp_segment_coord(df$wt, df$hp, intercept, slope) %>% 
+  segments <- perp_segment_coord(df[,x], df[,y], intercept, slope) %>% 
     as.data.frame()
   
-  plot <- df %>% ggplot(aes(x=wt, y=hp)) +
+  plot <- df %>% ggplot(aes_string(x=x, y=y)) +
     geom_segment(data=segments, 
                  aes(x=x0, y=y0, xend=x1, yend=y1), 
                  colour=get_hurwitz_colors("viking")) +
@@ -32,21 +57,23 @@ plot_perp <- function(df){
   plot
 }
 
-plot_reg_coord <- function(df){
-  df$wt <- df$wt / sd(df$wt)
-  df$hp <- df$hp / sd(df$hp)
+plot_reg_coord <- function(df, x, y){
+  df[,x] <- df[,x] / sd(df[,x])
+  df[,y] <- df[,y] / sd(df[,y])
   
   lm_model <- linear_reg() %>% 
     set_engine("lm") %>% 
     set_mode("regression")
   
+  form <- formula(paste(eval(y),"~", eval(x)))
+  
   lm_fit <- lm_model %>% 
-    fit(hp ~ wt, data=df)
+    fit(form, data=df)
   
   intercept <- summary(lm_fit$fit)$coefficients["(Intercept)",1]
-  slope <- summary(lm_fit$fit)$coefficients["wt",1]
+  slope <- summary(lm_fit$fit)$coefficients[x,1]
   
-  segments <- perp_segment_coord(df$wt, df$hp, intercept, slope) %>% 
+  segments <- perp_segment_coord(df[,x], df[,y], intercept, slope) %>% 
     as.data.frame()
   
   rot_df <- df
@@ -67,4 +94,36 @@ plot_reg_coord <- function(df){
     ))
   
   rot_df
+}
+
+#' get_coef
+#' Get coefficient from lm() fit
+#'
+#' @param ft Fit object from lm()
+#' @param var Variable for coefficient
+#'
+#' @return Coefficient
+#' @export
+#'
+#' @examples
+get_coef <- function(ft, var="(Intercept)") {
+  summary(ft$fit)$coefficients[var ,1]
+}
+
+
+
+#' get_rsq
+#' Get R-Squared from an lm() fit
+#'
+#' @param ft Fit object from lm()
+#' @param digits Digits for rounding
+#' 
+#' @return
+#' @export
+#'
+#' @examples
+get_rsq <- function(ft, digits=3) {
+  r_squared <- round(summary(ft$fit)$r.squared, digits=digits)
+  
+  return(r_squared)
 }
